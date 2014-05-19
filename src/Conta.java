a1dcc2e2e0755617f74581cbb7662a0884adb90b
deciomoritz <deciomoritz1@gmail.com>
2014-05-19 16:04:51 -0300

    hashCode and equals reescritos. Equal removido.

--- src/Conta.java
+++ src/Conta.java
@@ -8,8 +8,37 @@ public class Conta {
 		this.senha = senha;
 	}
 	
-	public boolean equal(Conta c) {
-		return login == c.login && senha == c.senha;
+	public Conta(){
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (senha == null) {
+			if (other.senha != null)
+				return false;
+		} else if (!senha.equals(other.senha))
+			return false;
+		return true;
+	}
 }
