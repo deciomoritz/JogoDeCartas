239b9754863dd55a8285e24bec18c9dc14004695
deciomoritz <deciomoritz1@gmail.com>
2014-05-19 01:45:40 -0300

    Construtor de acordo com diagrama

--- src/Conta.java
+++ src/Conta.java
@@ -3,7 +3,9 @@ public class Conta {
 	private String login;
 	private String senha;
 	
-	public Conta() {
+	public Conta(String login, String senha) {
+		this.login = login;
+		this.senha = senha;
 	}
 	
 	public boolean equal(Conta c) {
