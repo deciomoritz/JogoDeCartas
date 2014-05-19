085437d463bb29cf7185a7143180063a83580196
deciomoritz <deciomoritz1@gmail.com>
2014-05-19 18:31:09 -0300

    Acrescentados novos métodos em função da interface

--- src/Acervo.java
+++ src/Acervo.java
@@ -1,5 +1,3 @@
-
-
 import java.util.ArrayList;
 
 public class Acervo {
@@ -8,6 +6,14 @@ public class Acervo {
 	
 	public Acervo() {
 		listaDeCartas = new ArrayList<Carta>();
+		
+		Carta c1 = new Carta("The 5.75 terror", "Esta carta é ativada todo final de semestre", Tipo.ENCANTAMENTO);
+		Carta c2 = new Carta("The KAHSSIAN", "Esta carta é muito efetiva contra o tipo Estudante", Tipo.MONSTRO);
+		Carta c3 = new Carta("Diagrama de Sequencia", "Esta carta causa olheiras e sonolência", Tipo.MAGIA);
+		
+		listaDeCartas.add(c1);
+		listaDeCartas.add(c2);
+		listaDeCartas.add(c3);
 	}
 	
 	public ArrayList<Carta> mostrarAcervo() {
@@ -16,6 +22,14 @@ public class Acervo {
 			copiaLista.add(listaDeCartas.get(i));
 		}
 		return copiaLista;
+	}
+	
+	public Carta getCarta(int i){
+		return new Carta(listaDeCartas.get(i));
+	}
+	
+	public int quantidade(){
+		return listaDeCartas.size();
 	}
 
 }
