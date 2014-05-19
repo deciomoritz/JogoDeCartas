47558581b2396be3605b69f8d219cd866876f2d8
deciomoritz <deciomoritz1@gmail.com>
2014-05-19 01:19:03 -0300

    Métodos implementados

--- src/Conta.java
+++ src/Conta.java
@@ -1,17 +1,13 @@
-
-
 public class Conta {
 	
 	private String login;
 	private String senha;
 	
 	public Conta() {
-		// TODO
 	}
 	
 	public boolean equal(Conta c) {
-		// TODO
-		return false;
+		return login == c.login && senha == c.senha;
 	}
 
 }
