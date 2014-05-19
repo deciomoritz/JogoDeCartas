de9f8704e23abd1c57596ea96e6938f0ce0fe630
deciomoritz <deciomoritz1@gmail.com>
2014-05-19 18:30:53 -0300

    Acrescentados novos construtores

--- src/Carta.java
+++ src/Carta.java
@@ -4,14 +4,33 @@ public class Carta {
 	private String descricao;
 	private Tipo t;
 	
-	public Carta() {
-		//TODO
+	public Carta(){
 	}
 	
+	public Carta(String nome, String descricao, Tipo t) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.t = t;
+	}
+	
+	public Carta(Carta c){
+		this.nome = c.nome;
+		this.descricao = c.descricao;
+		this.t = c.t;
+	}
+
 	public String getNome() {
 		return nome;
 	}
 	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public String getT() {
+		return t.toString();
+	}
+
 	public ExemplarDeCarta getExemplar() {
 		return new ExemplarDeCarta(this);
 	}
