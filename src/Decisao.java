c05c716594da25b2386e1486db3d81007620fc81
deciomoritz <deciomoritz1@gmail.com>
2014-05-19 22:28:27 -0300

    Simplificado

--- src/Decisao.java
+++ src/Decisao.java
@@ -1,19 +1,4 @@
-
-
 public enum Decisao {
-	
-	NÃO_TOMADA(0),
-	REMOVER(1),
-	ADICIONAR(2),
-	TERMINAR(3);
-	
-	private int valor;
-	
-	Decisao(int v) {
-		valor = v;
-	}
-	
-	public int getValor() {
-		return this.valor;
-	}
+	REMOVER,
+	ADICIONAR
 }
